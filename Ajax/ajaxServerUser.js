117457560dc4962b344bbcbdbfcee76cfd8c138a
SUMITpatil2021 <sumit.patil2021@gmail.com>
2023-12-09 00:27:27 +0530

    .

--- Ajax/ajaxServerUser.js
+++ Ajax/ajaxServerUser.js
@@ -5,6 +5,8 @@ app.listen(9000,function(){
     console.log("Express Server started on 9000");
 });
 
+app.use(exp.static('resources'));
+
  var con=mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -12,16 +14,38 @@ app.listen(9000,function(){
     database:"knowit"
  })
 
- con.connect()
+ con.connect(function(err){
+    if(!err)
+    {
+        console.log("Database Connected ");
+    }
+    else
+    {
+        console.log("Database not connected ");
+    }
+ })
 
 app.get('/user',function(req,res){
-    res.sendFile(__dirname+"/registrationform.html")
-})
+    res.sendFile(__dirname+"/registrationform.html");
+});
 
 app.get('/ErrMsg',function(req,res){
-
+var user=req.query.uid;
+var query="select ename from emp where ename="+user;
+con.query(query,function(err,result){
+  
+    if(!err)
+    {   
+        if(user==query)
+        res.send("<h3> user name Already taken </h3>");
+    
+        else
+        res.send("<h3>Username available </h3>")
+    }
+   
+})
 })
 
 app.all('*',function(req,res){
-    res.send("<h3>Invalid URL</h3")
+    res.send("<h3>Invalid URL</h3");
 })
